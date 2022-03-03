d0bab8bf74db1ccac898144a48f2dfb726c73da0
lakhal oumaima <lakhalouma6@gmail.com>
2022-03-03 17:22:21 +0100

    commit 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,33 @@ class PostsController < ApplicationController
     def show
         render json: @post
     end
+    def create 
+        @post = Post.new(post_params)
+        if @post.save 
+            render json: @post ,statut: :created, location: @post 
+       
+        else
+            render json: @post.errors, statut: :unprocessable_entity
+        end    
+    end    
 
-
+    def update 
+        if @post.update(post_params)
+            render json: @post 
+       
+        else
+            render json: @post.errors, statut: :unprocessable_entity
+        end    
+    end
+    def destroy 
+        @post.destroy
+    end
     private 
 
     def set_post 
         @post = Post.find(params[:id])
     end    
+    def post_params 
+        params.require(:post).permit!
+    end
 end
