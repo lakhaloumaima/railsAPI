b13e27eb644a260c7fc95f93c64528294ffab7c6
lakhal oumaima <lakhalouma6@gmail.com>
2022-03-03 14:13:36 +0100

    second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
+    before_action :set_post ,only:[:show , :update ,:destroy]
     def index
         @posts = Post.all
         render json: @posts
     end
-    
+    def show
+        render json: @post
+    end
+
+
+    private 
+
+    def set_post 
+        @post = Post.find(params[:id])
+    end    
 end
